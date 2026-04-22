3977fabb3fdf2b543405deed70ae2c65e914649c
Soumya Khan <soumyakhan265.com>
2026-04-22 10:25:37 +0000

    added the postgres setings in the database

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,12 +9,12 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     '''Test the calc modules'''
-    
+
     def test_add_numbers(self):
         '''Test adding numbers together.'''
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-   
+
     def test_subtract_numbers(self):
         '''Test subtracting numbers together.'''
         res = calc.subtract(11, 6)
